9c5c29d7f7fb4c2d6ffaa8d419d2f706c87338b9
qiqi <1521371275@qq.com>
2020-05-06 14:38:46 +0800

    初始化项目

---
+++ rainbow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rainbow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#.NET!");
+        }
+    }
+}
