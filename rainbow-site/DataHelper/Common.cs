3c57e9875a43596366c96fd1dcdd390fecf6f596
eatrice <1521371275@qq.com>
2020-08-20 22:52:39 +0800

    add Qixi - type1 and type2

--- rainbow-site/DataHelper/Common.cs
+++ rainbow-site/DataHelper/Common.cs
@@ -7,8 +7,8 @@ namespace rainbow_site
         public static string backgroundImg;
         public static DateTime date = DateTime.Now;
         public static void InitBGImg(){
-            string[] filePaths = FileHelper.GetFileList("wwwroot/img/mainBackground/");
-            backgroundImg = "/img/mainBackground/" + filePaths[new Random().Next(0, filePaths.Length)];
+            string[] filePaths = FileHelper.GetFileList("wwwroot/img/MainBackground/");
+            backgroundImg = "/img/MainBackground/" + filePaths[new Random().Next(0, filePaths.Length)];
         }
         /// <summary>
         /// 每天更新背景图片
